a436a9bd6e7bf357a3037ebaa019100fe6ce8860
박현민 <dev.hmpark@gmail.com>
2024-04-10 17:07:54 +0900

    feat: 회원 설정 페이지 생성

---
+++ src/pages/SettingPage/SettingPage.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const SettingPageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SettingPageContent = styled.div`
+  width: 935px;
+  /* background-color: white; */
+  margin-top: 10px;
+`;
