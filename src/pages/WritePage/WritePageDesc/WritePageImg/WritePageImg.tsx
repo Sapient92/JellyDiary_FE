a7fe52443dba433bf87c90f44f3db087c53b770d
Sapient92 <angels6312@naver.com>
2024-04-12 12:19:31 +0900

    [JD-63]Feat: 이미지 여러장 업로드 및 미리보기

--- src/pages/WritePage/WritePageDesc/WritePageImg/WritePageImg.tsx
+++ src/pages/WritePage/WritePageDesc/WritePageImg/WritePageImg.tsx
@@ -10,10 +10,10 @@ import * as React from "react";
 import { useRef, useState } from "react";
 
 const WritePageImg = () => {
-  const [postImg, setPostImg] = useState<File[]>([]);
-  const [previewImg, setPreviewImg] = useState<string[]>([]);
+  const [postList, setPostList] = useState<
+    { id: number; previewUrl: string; origin: File }[]
+  >([]);
   const imgRef = useRef<HTMLInputElement>(null);
-  console.log(postImg);
   const handleUploadClick = () => {
     if (!imgRef.current) {
       return;
@@ -24,34 +24,55 @@ const WritePageImg = () => {
   const uploadImgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let fileArr = e.target.files;
     if (fileArr) {
-      setPostImg(Array.from(fileArr));
-      let fileRead = new FileReader();
-      fileRead.readAsDataURL(fileArr[0]);
-      fileRead.onload = () => {
-        if (typeof fileRead.result === "string") {
-          setPreviewImg([...previewImg, fileRead.result]);
-        } else {
-          console.error("fileRead.result is not a string");
-        }
-      };
+      const newFilesArray = Array.from(fileArr);
+      newFilesArray.forEach((file) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+          if (typeof fileReader.result === "string") {
+            setPostList((prev) => [
+              ...prev,
+              {
+                id: file.lastModified,
+                previewUrl: fileReader.result as string,
+                origin: file,
+              },
+            ]);
+          } else {
+            console.error("fileRead.result is not a string");
+          }
+        };
+        fileReader.readAsDataURL(file);
+      });
     }
+  };
+
+  const handleLeftClick = () => {
+    document.getElementById("imgContainer")!.scrollLeft -= 420;
+  };
+
+  const handleRightClick = () => {
+    document.getElementById("imgContainer")!.scrollLeft += 420;
   };
 
   return (
     <WritePageImgContainer>
       <WritePageImgTitleContainer>
-        <p>사진(1/5)</p>
+        <p>사진({postList.length}/5)</p>
         <p>오늘 하루를 사진으로 기록해요.</p>
       </WritePageImgTitleContainer>
       <WritePageImgContent>
-        <PreviewImgContainer>
-          {previewImg.length !== 0 && (
-            <img src={previewImg[0]} alt={"upload_img"} />
-          )}
+        {postList.length >= 2 && <span onClick={handleLeftClick}>{"<"}</span>}
+        <PreviewImgContainer id={"imgContainer"}>
+          {postList.length !== 0 &&
+            postList.map((post) => (
+              <img key={post.id} src={post.previewUrl} alt={"uploadImg"} />
+            ))}
         </PreviewImgContainer>
+        {postList.length >= 2 && <span onClick={handleRightClick}>{">"}</span>}
         <input
           type={"file"}
           accept={"image/*"}
+          multiple={true}
           ref={imgRef}
           onChange={uploadImgChange}
         />
