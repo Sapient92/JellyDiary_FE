bf5c0060b8e6cd6fde31eb67a921ebedbb0de1f4
Sapient92 <angels6312@naver.com>
2024-04-18 11:45:22 +0900

    [JD-53]Feat: header에 수정, 삭제 버튼 보여주는 모달 생성

--- src/pages/PostPage/PostPageHeader/PostPageHeader.styles.ts
+++ src/pages/PostPage/PostPageHeader/PostPageHeader.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 export const PostPageHeaderContainer = styled.header`
   width: 100%;
@@ -25,13 +26,30 @@ export const UserProfileContainer = styled.div`
   }
 `;
 
+export const HeaderButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 export const PostMenuButton = styled.button`
   color: black;
   margin-right: 20px;
   background: none;
   border: none;
+  cursor: pointer;
   img {
     width: 15px;
     height: 15px;
   }
+`;
+
+export const EditButtonContainer = styled.div`
+  position: relative;
+`;
+
+export const EditButton = styled(HiOutlineDotsHorizontal)`
+  margin-right: 20px;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
 `;
