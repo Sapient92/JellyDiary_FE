deb097f32a56279ca625dec28a64f5af7119b8d4
박현민 <dev.hmpark@gmail.com>
2024-04-19 19:24:28 +0900

    feat: LoginPage 생성

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ import ChatPage from "./pages/ChatPage/ChatPage.tsx";
 import DiaryPage from "./pages/DiaryPage/DiaryPage.tsx";
 import DemoApp from "./pages/DiaryPage/DiaryTestPage.tsx";
 import { ReactQueryDevtools } from "react-query/devtools";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Layout />}>
+          <Route path={"/login"} element={<LoginPage />} />
           <Route path={"/userfeed"} element={<UserFeedPage />} />
           <Route path={"/post/:id"} element={<PostPage />} />
           <Route path={"/write"} element={<WritePage />} />
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import login_K from "../../assets/button/Login_K.png";
+import login_N from "../../assets/button/Login_N.png";
+import login_G from "../../assets/button/Login_G.png";
+import login_F from "../../assets/button/Login_F.png";
+import login_Line from "../../assets/button/Login_Line.png";
+
+import {
+  LoginPageContainer,
+  LoginPageContent,
+  LoginButton,
+} from "./LoginPage.styles";
+
+const LoginPage = () => {
+  return (
+    <LoginPageContainer>
+      <LoginPageContent>
+        <LoginButton>
+          <img src={login_K} />
+        </LoginButton>
+        <img src={login_Line} />
+        <LoginButton>
+          <img src={login_N} />
+          <span>네이버 로그인 </span>
+        </LoginButton>
+        <LoginButton>
+          <img src={login_G} />
+          <span>Google 로그인 </span>
+        </LoginButton>
+        <LoginButton>
+          <img src={login_F} />
+          <span> Facebook 로그인</span>
+        </LoginButton>
+      </LoginPageContent>
+    </LoginPageContainer>
+  );
+};
+
+export default LoginPage;
