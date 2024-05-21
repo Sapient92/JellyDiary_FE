c85c2c2be07ef22db251f9aab797765b7fad72a5
Sapient92 <angels6312@naver.com>
2024-05-21 09:14:22 +0900

    [JD-14]Refactor: 멘션 리팩토링
    [JD-14]Refactor: 멘션 리팩토링

--- src/components/Modal/CommentModal/CommentContent/CommentContent.styles.ts
+++ src/components/Modal/CommentModal/CommentContent/CommentContent.styles.ts
@@ -1,8 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const CommentContentContainer = styled.ul`
+export const CommentContentContainer = styled.ul<{ $comment: boolean }>`
   list-style: none;
   padding: 0;
   max-height: 700px;
   overflow-y: auto;
+  ${(props) =>
+    !props.$comment &&
+    css`
+      p:nth-child(1) {
+        text-align: center;
+        font-weight: 600;
+        font-size: 18px;
+        margin-top: 50px;
+      }
+    `}
 `;
--- src/components/Modal/MentionModal/index.tsx
+++
@@ -1,7 +0,0 @@
-import { MentionModalContainer } from './MentionModal.styles.ts';
-
-const MentionModal = () => {
-  return <MentionModalContainer></MentionModalContainer>;
-};
-
-export default MentionModal;
