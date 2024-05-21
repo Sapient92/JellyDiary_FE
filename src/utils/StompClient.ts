e90f10e09f6e9e10abdc67d0fa4164a6a86b3b77
Sapient92 <angels6312@naver.com>
2024-05-21 19:53:39 +0900

    Update StompClient.ts

--- src/utils/StompClient.ts
+++ src/utils/StompClient.ts
@@ -2,9 +2,9 @@ import { Client } from "@stomp/stompjs";
 
 export const client = new Client({
   brokerURL: "wss://api.jellydiary.life/ws/chat",
-  debug: (string) => {
-    console.log(string);
-  },
+  // debug: (string) => {
+  //   console.log(string);
+  // },
   onStompError: (frame) => {
     console.log(frame);
   },
